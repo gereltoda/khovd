bab73463d6a75a2bf980bf90a3f53142164e849b
khovd <gereltod1988@gmail.com>
2016-12-19 23:39:49 +0700

    za solilloo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    soligdson
+                    ingej  solii
                 </div>
             </div>
         </div>
