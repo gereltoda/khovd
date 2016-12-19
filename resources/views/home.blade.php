9bb804b42b90977a7ade53df95eab15d99955a5e
khovd <gereltod1988@gmail.com>
2016-12-19 23:50:21 +0700

    gerlee

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    ingej  solii
+                    ingej  solii-gerlee
                 </div>
             </div>
         </div>
